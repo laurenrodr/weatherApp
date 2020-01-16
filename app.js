815f93298f1c569bfee74c96102baa837afa1744
Lauren Rodriguez <laurenrodriguez729@gmail.comm>
2020-01-16 18:21:52 -0500

    update sunset and sunrise

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ window.addEventListener("load", function(){
     let tempDegrees = document.querySelector('.temp-degrees');
     let locationTimezone = document.querySelector('.location-timezone');
     let degreesSection = document.querySelector('.degrees');
-    const tempSpan = document.querySelector('.degrees span')
+    const tempSpan = document.querySelector('.degrees span');
+    let riseTime = document.querySelector('.sunrise');
+    let setTime = document.querySelector('.sunset');
+
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position){
             lon = position.coords.longitude;
             lat = position.coords.latitude;
+
+            console.log("lon: " + lon);
+            console.log("lat: " + lat);
+
 
             const proxy = `https://cors-anywhere.herokuapp.com/`;
             const api = `${proxy}https://api.darksky.net/forecast/1ca47e16f27bc9d6c9f9a13024ad1a88/${lat},${lon}`;
@@ -23,6 +30,7 @@ window.addEventListener("load", function(){
                     return response.json();
                 })
                 .then(function(data){
+                    console.log(data);
                     const {temperature, summary, icon} = data.currently;
 
                     //set DOM elements from darksky API
@@ -53,8 +61,28 @@ window.addEventListener("load", function(){
                             tempDegrees.textContent = Math.floor(temperature);
                         }
                     });
+                    setRise(lat, lon);
                 });
         });
+    }
+
+    function setRise(lat, lon){
+        //fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=today`)
+        fetch(`https://api.sunrise-sunset.org/json?lat=25.7742658&lng=-80.1936589&formatted=0`)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(sun){
+                const {sunrise, sunset} = sun.results;
+                let utcRise = sunrise;
+                let utcSet = sunset;
+
+                let rise = new Date(utcRise);
+                let set = new Date(utcSet);
+
+                riseTime.textContent = rise.toString().substring(16, 21);
+                setTime.textContent = set.toString().substring(16, 21);
+            })
     }
 
     function setIcons(icon, iconID){
